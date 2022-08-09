d5b172fcb6089f7a27a2818223ee91b579fcedc5
Jonas Achouri Sihlen <jonasasihlen@gmail.com>
2022-08-09 16:06:02 +0200

    Adding method that responds to GET requests so that countries can be retrieved.

--- src/main/java/com/example/TravelAdventuresAPI/Controller/TravelAdventuresController.java
+++ src/main/java/com/example/TravelAdventuresAPI/Controller/TravelAdventuresController.java
@@ -3,9 +3,9 @@ package com.codecademy.plants.controllers;
 import com.codecademy.plants.entities.Adventure;
 import com.codecademy.plants.repositories.AdventureRepository;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
 
 
 @RestController ("/traveladventures")
@@ -21,6 +21,12 @@ public class TravelAdventuresController {
     @GetMapping()
     public Iterable<Adventure> getAllAdventures(){
         return this.adventureRepository.findAll();
+    }
+
+    @GetMapping("/bycountry/{country}")
+    public List<Adventure> getByCountry(@PathVariable("country") String
+                                                country) {
+        return this.adventureRepository.findByCountry(country);
     }
 
 }
